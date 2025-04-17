14399a79838e5d28b48b6e5198cb55e28e208fab
sandanam <santhanamk9604@gmail.com>
2025-04-17 09:59:49 +0530

    added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const jwt = require('jsonwebtoken');
+
 const encrypt = (payload, secret) => {
   // your code here and return token
+  const options = {expiresIn:'1h'};
+
+  return jwt.sign(payload, secret, options)
 };
 
 module.exports = encrypt;
